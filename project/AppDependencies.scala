1fc922cf5b6ca0e76c6e83d6bb85f83a4069f058
sureshhmrc <27204388+sureshhmrc@users.noreply.github.com>
2025-03-11 16:04:55 +0000

    bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "9.11.0"
-  private val hmrcMongoVersion = "2.2.0"
+  private val hmrcMongoVersion = "2.5.0"
   private val playVersion = "play-30"
 
   val compile: Seq[ModuleID] = Seq(
